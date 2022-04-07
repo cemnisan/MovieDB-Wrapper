44e6aee9fa2c7b7c3d731644360fb1a454cf30af
cemnisan <2017417057@ogr.cu.edu.tr>
2022-04-07 17:24:24 +0300

    refactor: similar movies model

--- Sources/MovieDB-Wrapper/Models/Movies/SimilarMovies.swift
+++ Sources/MovieDB-Wrapper/Models/Movies/SimilarMovies.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct SimilarMovies: Codable
 {
     public let page: Int?
-    public let results: [SimilarMoviesResult]?
+    public let results: [Movies]?
     public let totalPages, totalResults: Int?
 }
 
@@ -21,38 +21,5 @@ extension SimilarMovies
         case page, results
         case totalPages = "total_pages"
         case totalResults = "total_results"
-    }
-}
-
-public struct SimilarMoviesResult: Codable
-{
-    let adult: Bool?
-    let backdropPath: String?
-    let genreIDS: [Int]?
-    let id: Int?
-    let title, originalLanguage, originalTitle, overview: String?
-    let popularity: Double?
-    let posterPath, releaseDate: String?
-    let video: Bool?
-    let voteAverage: Double?
-    let voteCount: Int?
-}
-
-extension SimilarMoviesResult
-{
-    public enum CodingKeys: String, CodingKey
-    {
-        case adult
-        case backdropPath = "backdrop_path"
-        case genreIDS = "genre_ids"
-        case id, title
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case overview, popularity
-        case posterPath = "poster_path"
-        case releaseDate = "release_date"
-        case video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
     }
 }
